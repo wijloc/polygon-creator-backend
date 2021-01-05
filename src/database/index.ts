37ff44824d0f0aac7a3bb7f1b94f5fc303422035
Willian Oliveira <wijloc@gmail.com>
2021-01-05 17:29:31 -0300

    refactor: remove ormconfig and create connection options to use URL

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,3 +1,30 @@
-import { createConnection } from 'typeorm';
+import { ConnectionOptions, createConnection } from 'typeorm';
 
-createConnection();
+const getOptions = async () => {
+  const connectionOptions: ConnectionOptions = {
+    type: 'postgres',
+    entities: ['./src/models/*.ts'],
+    migrations: ['./src/database/migrations/*.ts'],
+    synchronize: false,
+    logging: false,
+    host: 'localhost',
+    port: 5432,
+    username: 'postgres',
+    password: 'postgres',
+    database: 'polygoncreator',
+  };
+  if (process.env.DATABASE_URL) {
+    Object.assign(connectionOptions, { url: process.env.DATABASE_URL });
+  }
+
+  return connectionOptions;
+};
+
+const connectDatabase = async (): Promise<void> => {
+  const typeormconfig = await getOptions();
+  createConnection(typeormconfig);
+};
+
+connectDatabase().then(async () => {
+  console.log('Connected to database');
+});
