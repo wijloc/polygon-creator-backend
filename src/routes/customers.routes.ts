7c0329982f6c6c97b01fc222aee86f203190b4a4
Willian Oliveira <wijloc@gmail.com>
2021-02-27 15:44:35 -0300

    feat: route to get customers from polygon

--- src/routes/customers.routes.ts
+++ src/routes/customers.routes.ts
@@ -1,8 +1,19 @@
 import { Router } from 'express';
+import { getRepository } from 'typeorm';
 
 import CreateCustomersService from '../services/CreateCustomersService';
 
+import Customer from '../models/Customer';
+
 const customersRouter = Router();
+
+customersRouter.get('/:polygon_id', async (request, response) => {
+  const customersRepository = getRepository(Customer);
+  const customers = await customersRepository.find({
+    where: { polygon_id: request.params.polygon_id },
+  });
+  return response.json(customers);
+});
 
 customersRouter.post('/', async (request, response) => {
   const { polygon_id, customers } = request.body;
